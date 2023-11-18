44d25dbf828827ba948d9cd9cc35a45c08fe7c80
Supriya Ponnaluru <sponnaluru@ucsb.edu>
2023-11-17 18:28:09 -0800

    Added socket-io basic functionality

--- app.py
+++ app.py
@@ -1,42 +1,51 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask_cors import CORS
+from uuid import uuid4
 
 app = Flask(__name__)
 app.secret_key = '1234567890abcdef1234567890abcdef'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 db = SQLAlchemy(app)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+@socketio.on('join_board')
+def on_join(data):
+    room = data['board_id']
+    join_room(room)
+    # emit('status', {'msg': f'Someone has joined the board {room}.'}, room=room)
 
-    def __repr__(self):
-        return f'<User {self.username}>'
-    
+@socketio.on('update_board')
+def on_update_board(data):
+    room = data['board_id']
+    emit('board_updated', data, room=room, include_self=False)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/create_board', methods=['POST'])
 def create_board():
-   # Example of retrieving form data
+# Example of retrieving form data
     retro_type = request.form['retroType']
     team_size = request.form['teamSize']  # make sure these match your form input names
-
-    # Logic to handle the data...
-    # For example, storing the selected type in the session
     session['retro_type'] = retro_type
-    return redirect(url_for('board'))
+    unique_id = str(uuid4())
+    return redirect(url_for('board', board_id=unique_id))
+
+@app.route('/board/<board_id>')
+def board(board_id):
+    return render_template('board.html', board_id=board_id)
 
 @app.route('/board')
-def board():
+def board_defaukt():
     retro_type = session.get('retro_type', 'default_type')
     return render_template('board.html', retro_type=retro_type)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 migrate = Migrate(app, db)
--- static/scripts.js
+++ static/scripts.js
@@ -13,26 +13,26 @@ function createStickyNote() {
     stickyNote.id = 'sticky_' + Date.now(); // Assign a unique ID
 
     // Set up the dragstart event
-    stickyNote.addEventListener('dragstart', function(event) {
+    stickyNote.addEventListener('dragstart', function (event) {
         event.dataTransfer.setData('text/plain', stickyNote.id);
     });
 
     stickyNote.innerHTML = 'Add your comments'; // Placeholder text with a bullet point
 
-    stickyNote.addEventListener('focus', function(event) {
+    stickyNote.addEventListener('focus', function (event) {
         if (stickyNote.innerHTML.trim() === 'Add your comments') {
             stickyNote.innerHTML = '<div>•&nbsp;</div>'; // Start with a bullet point on a new line
             setCaretToEnd(stickyNote); // Set caret after the bullet point
         }
     });
 
-    stickyNote.addEventListener('blur', function(event) {
+    stickyNote.addEventListener('blur', function (event) {
         if (!stickyNote.textContent.trim()) {
             stickyNote.innerHTML = 'Add your comments'; // Re-add placeholder text with a bullet point
         }
     });
 
-    stickyNote.addEventListener('keydown', function(event) {
+    stickyNote.addEventListener('keydown', function (event) {
         // Check if the Enter key is pressed
         if (event.key === 'Enter') {
             event.preventDefault(); // Prevent the default enter behavior
@@ -70,15 +70,15 @@ function setCaretToEnd(contentEditableElement) {
 function enableDropZones() {
     var dropZones = document.querySelectorAll('.sticky-notes-container');
 
-    dropZones.forEach(function(dropZone) {
+    dropZones.forEach(function (dropZone) {
         // Set up the dragover event
-        dropZone.addEventListener('dragover', function(event) {
+        dropZone.addEventListener('dragover', function (event) {
             event.preventDefault(); // Necessary to allow a drop
             // Optional: Add some visual feedback
         });
 
         // Set up the drop event
-        dropZone.addEventListener('drop', function(event) {
+        dropZone.addEventListener('drop', function (event) {
             event.preventDefault(); // Prevent default behavior
             var id = event.dataTransfer.getData('text/plain');
             var draggableElement = document.getElementById(id);
@@ -87,3 +87,37 @@ function enableDropZones() {
         });
     });
 }
+
+var socket = io.connect(window.location.origin);
+
+socket.on('connect', function () {
+    // Join the board room
+    socket.emit('join_board', { board_id: boardId });
+});
+
+socket.on('board_updated', function(data) {
+    // Assuming data has noteId, content, and position
+    let note = document.getElementById(data.noteId);
+    if (!note) {
+        // If the note doesn't exist, create it
+        note = document.createElement('div');
+        note.id = data.noteId;
+        note.classList.add('sticky-note');
+        note.contentEditable = true;
+        document.querySelector('.board-container').appendChild(note);  // Adjust this selector based on your HTML structure
+    }
+
+    // Update the note's content and position
+    note.innerText = data.content;
+    note.style.left = data.position.x + 'px';
+    note.style.top = data.position.y + 'px';
+});
+
+
+function sendBoardUpdate(updateData) {
+    socket.emit('update_board', { board_id: boardId, ...updateData });
+}
+
+window.onload = function () {
+    enableDropZones();
+};
